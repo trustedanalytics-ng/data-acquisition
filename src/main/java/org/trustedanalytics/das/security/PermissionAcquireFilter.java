8aad10bf3021028fa5112d0005165de207a8591e
Jacek Gerbszt <jacek.gerbszt@intel.com>
2016-12-21 17:21:56 +0100

    Use isEmpty instead of size > 0

--- src/main/java/org/trustedanalytics/das/security/PermissionAcquireFilter.java
+++ src/main/java/org/trustedanalytics/das/security/PermissionAcquireFilter.java
@@ -67,7 +67,7 @@ public class PermissionAcquireFilter extends OncePerRequestFilter {
                     .map(p -> p.getOrg().getGuid().toString()).collect(Collectors.toList());
             request.setAttribute(ACCESSIBLE_ORGS, ids);
 
-            if (ids.size() > 0) {
+            if (!ids.isEmpty()) {
                 filterChain.doFilter(request, httpServletResponse);
             } else {
                 LOGGER.debug("User access denied.");
